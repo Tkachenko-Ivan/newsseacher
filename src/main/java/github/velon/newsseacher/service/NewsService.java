7a811ca315d16cad836910f1a39a68e688e32153
Иван <ower-7@yandex.ru>
2022-12-07 11:33:58 +0500

    Вставляем данные в real-time индекс

--- src/main/java/github/velon/newsseacher/service/NewsService.java
+++ src/main/java/github/velon/newsseacher/service/NewsService.java
@@ -5,14 +5,19 @@ import github.velon.newsseacher.repository.NewsRepository;
 import java.sql.SQLException;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,6 +27,8 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service("NewsService")
 public class NewsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NewsService.class);
 
     NewsRepository newsRepository;
 
@@ -38,7 +45,24 @@ public class NewsService {
     }
 
     public News addNews(News news) {
-        return newsRepository.save(news);
+        // Сохранение в хранилище
+        News n = newsRepository.save(news);
+
+        // Вставка в поисковый индекс
+        try {
+            String sql = """
+                     INSERT INTO news_rt (id, find_headline, find_content_text, headline, content_text, post_date)
+                     VALUES (?,?,?,?,?,?)      
+                     """;
+            Long seconds = null;
+            if (n.getPostDate() != null) {
+                seconds = n.getPostDate().toEpochSecond(LocalTime.NOON, ZoneOffset.MIN);
+            }
+            manticoreJdbcTemplate.update(sql, n.getId(), n.getHeadline(), n.getContent(), n.getHeadline(), n.getContent(), seconds);
+        } catch (DataAccessException ex) {
+            logger.error(ex.getMessage());
+        }
+        return n;
     }
 
     public News updateNews(Long id, News news) {
@@ -54,7 +78,7 @@ public class NewsService {
         String sql
                 = """
                 SELECT * 
-                FROM news_sql
+                FROM news
                 WHERE match (?) 
                 ORDER BY weight() desc 
                 """;
